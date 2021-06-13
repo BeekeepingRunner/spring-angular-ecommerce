1f00a3801ceeb161503fb8f5eb844e52cb8c587d
Bartosz Biegajło <biegbart.z@gmail.com>
2021-06-13 21:05:33 +0200

    show product category above products section

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
@@ -12,12 +13,14 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number;
+  currentCategoryName: string;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute)
     { 
       this.currentCategoryId = 1;
+      this.currentCategoryName = "Books";
     }
 
   ngOnInit(): void {
@@ -30,11 +33,17 @@ export class ProductListComponent implements OnInit {
   listProducts() {
 
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    const hasCategoryName: boolean = this.route.snapshot.paramMap.has('name');
 
-    if (hasCategoryId) {
+    if (hasCategoryId && hasCategoryName) {
       this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+      let temp = this.route.snapshot.paramMap.get('name')?.toString();
+      if (temp != null) {
+        this.currentCategoryName = temp.toString();
+      }
     } else {
       this.currentCategoryId = 1;
+      this.currentCategoryName = "Books";
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
@@ -43,5 +52,4 @@ export class ProductListComponent implements OnInit {
       }
     );
   }
-
 }
