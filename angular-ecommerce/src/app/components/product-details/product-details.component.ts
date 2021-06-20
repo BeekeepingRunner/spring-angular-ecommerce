611687a4736939947a1e150a7ba3cd716f9a7e78
Bartosz Biegajło <biegbart.z@gmail.com>
2021-06-20 20:08:08 +0200

    use product service to retrieve product details

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route : ActivatedRoute,
+    private productService : ProductService,
+    private productId : number,
+    public product : Product
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      () => {
+        this.handleProductDetails();
+      }
+    )
   }
 
+  handleProductDetails() {
+    if (this.route.snapshot.paramMap.has('id')) {
+
+      this.productId = Number(this.route.snapshot.paramMap.get('id'));
+      this.productService.getProduct(productId).subscribe(
+        data => {
+          this.product = data;
+        }
+      );
+    }
+  }
 }
