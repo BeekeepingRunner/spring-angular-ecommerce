33da6c48f2483fca8de2e99b21380c7b2d3381cd
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-08-06 19:10:13 +0200

    prepare cart service

---
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import {CartItem} from "../common/cart-item";
+import {Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(newCartItem: CartItem) {
+
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem | undefined = undefined;
+
+    for (let cartItem of this.cartItems) {
+      if (cartItem.id === newCartItem.id) {
+        alreadyExistsInCart = true;
+        existingCartItem = cartItem;
+        break;
+      }
+    }
+
+    if (alreadyExistsInCart) {
+      // @ts-ignore
+      existingCartItem.quantity++;
+    }
+    else {
+      this.cartItems.push(newCartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    this.cartItems.forEach(cartItem => {
+      // @ts-ignore
+      totalPriceValue += cartItem.quantity * cartItem.unitPrice;
+      totalQuantityValue += cartItem.quantity;
+    });
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  }
+}
