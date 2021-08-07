35cb4a1cc10b4c606151bd0b285f59d339b39a06
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-08-07 13:02:57 +0200

    populate options for card expiration date

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {ShopFormService} from "../../services/shop-form.service";
 
 @Component({
   selector: 'app-checkout',
@@ -10,10 +11,19 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
 
+  creditCardYears: number[] = [];
+  //creditCardMonths: number[] = [];
+  monthNumbers: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+  ];
+
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private shopFormService: ShopFormService,
+    ) {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
@@ -46,7 +56,13 @@ export class CheckoutComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const startMonth: number = new Date().getMonth() + 1;
 
+    this.shopFormService.getCreditCardYears().subscribe(
+      data => {
+        this.creditCardYears = data;
+      }
+    );
   }
 
   onSubmit() {
